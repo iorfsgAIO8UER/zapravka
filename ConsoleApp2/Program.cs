894b2063a8e764f23023feeb8e34853dbe3ca8e3
school <school@A42PC6>
2025-02-25 14:08:07 +0200

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("=== Nova Poshta Simulation ===");
+            Console.WriteLine("1. Display Mail Services");
+            Console.WriteLine("2. Add/Edit/Delete Services");
+            Console.WriteLine("3. Accept Order for Shipment");
+            Console.WriteLine("4. Mark Order as Fulfilled");
+            Console.WriteLine("5. Save and Exit");
+            Console.Write("Choose an option: ");
+           
